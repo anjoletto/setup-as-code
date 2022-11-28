a5892a0c6a3b6809484c2ac99d27f6aa826c02ea
Leonardo Anjoletto Ferreira <43471239+anjoletto@users.noreply.github.com>
2022-11-28 07:29:24 -0300

    refac

--- nixos/users.nix
+++ nixos/users.nix
@@ -5,52 +5,53 @@
     extraGroups = [ "wheel" "audio" ];
     shell = pkgs.zsh ;
     packages = with pkgs; [
-	# system 
-	xorg.xhost
-	gotop
-	# utils
-	wget
-	playerctl
-	pamixer
-	magic-wormhole
-	ansible
-	# programming 
-	gitAndTools.gh
-	silver-searcher
-	python310
-	python310Packages.bpython
-	nodePackages.pyright
-	pipenv
-	black
-	lua53Packages.lua
-	clang
-	gcc
-	nodejs
-	tree-sitter
-	yarn
-	# GUI applications
-	lxqt.lxqt-policykit
-	gtk3-x11
-	xidlehook
-	polybar
-	sxhkd
-	i3lock
-	arandr
-	alacritty
-	pcmanfm
-	maim
-	pavucontrol
-	dunst
-	rofi
-	networkmanager
-	networkmanagerapplet
-	qutebrowser
-	librewolf
-	brave
-	unicode-emoji
-	spotify
-	discord
-	authy
+      # system
+      xorg.xhost
+      # utils
+      wget
+      playerctl
+      pamixer
+      magic-wormhole-rs
+      ansible
+      gotop
+      # programming
+      gitAndTools.gh
+      silver-searcher
+      python310
+      python310Packages.bpython
+      nodePackages.pyright
+      pipenv
+      black
+      lua53Packages.lua
+      clang
+      gcc
+      nodejs
+      tree-sitter
+      detox
+      yarn
+      # GUI applications
+      lxqt.lxqt-policykit
+      gtk3-x11
+      xidlehook
+      polybar
+      sxhkd
+      i3lock
+      arandr
+      alacritty
+      pcmanfm
+      maim
+      pavucontrol
+      dunst
+      rofi
+      networkmanager
+      networkmanagerapplet
+      qutebrowser
+      librewolf
+      brave
+      unicode-emoji
+      spotify
+      discord
+      authy
     ];
   };
 }
